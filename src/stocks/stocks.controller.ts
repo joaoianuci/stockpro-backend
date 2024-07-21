52b3a19e6e76539de84c1c8928231c6a0c8cc82c
João Vitor Ianuci <joao.mazzafc@contabilizei.com.br>
2024-07-21 12:44:58 -0300

    fix: remove unused imports

--- src/stocks/stocks.controller.ts
+++ src/stocks/stocks.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Inject, Param } from "@nestjs/common";
 import { ShowDetailedStockUseCase } from "./use-cases/show-detailed-stock.use-case";
 import { ListStocksUseCase } from "./use-cases/list-stocks.use-case";
-import { EventPattern, Payload } from "@nestjs/microservices";
 
 @Controller("stocks")
 export class StocksController {
